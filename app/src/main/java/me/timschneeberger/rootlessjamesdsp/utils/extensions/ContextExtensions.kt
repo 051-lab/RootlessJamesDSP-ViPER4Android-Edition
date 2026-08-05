db5e34cc91440a0ab22e46ade10c9df51779eba7
alienware377 <alienware377@users.noreply.github.com>
2026-08-05 06:37:39 +0000

    Allow .v4a release package in launch self-check

--- app/src/main/java/me/timschneeberger/rootlessjamesdsp/utils/extensions/ContextExtensions.kt
+++ app/src/main/java/me/timschneeberger/rootlessjamesdsp/utils/extensions/ContextExtensions.kt
@@ -170,7 +170,7 @@ object ContextExtensions {
     // Very simple & naive app cloner checks; please don't use multiple instances at once
     private val PKGNAME_REFS = setOf("bWUudGltc2NobmVlYmVyZ2VyLnJvb3RsZXNzamFtZXNkc3A=",
         "bWUudGltc2NobmVlYmVyZ2VyLnJvb3RsZXNzamFtZXNkc3AuZGVidWc=",
-        "amFtZXMuZHNw", "amFtZXMuZHNwLmRlYnVn")
+        "amFtZXMuZHNw", "amFtZXMuZHNwLmRlYnVn", "bWUudGltc2NobmVlYmVyZ2VyLnJvb3RsZXNzamFtZXNkc3AudjRh")
     private val APPNAME_REFS = setOf("Um9vdGxlc3NKYW1lc0RTUA==", "SmFtZXNEU1A=", "Um9vdGxlc3NWaVBFUjRBbmRyb2lk")
     fun Context.check(): Int {
         val appName = getAppName()
