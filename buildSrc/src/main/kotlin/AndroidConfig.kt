ae22775339f18a906da6f4dbf0cdd10429a3695d
soloarch <soloarch_io@gmail.com>
2026-08-13 17:09:21 -0500

    version: fork-mark merged build as 2.14.1-liveprog so this edition is distinguishable from upstream releases

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -5,7 +5,7 @@ object AndroidConfig {
     const val targetSdk = 34
 
     // Version of this fork (RootlessViPER4Android)
-    const val versionName = "2.14.1"
+    const val versionName = "2.14.1-liveprog"
     const val versionCode = 86
 
     // Upstream RootlessJamesDSP release this fork is based on
